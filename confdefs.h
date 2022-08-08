3805a7d0599549674bc14e4b6179b4a1417d6c83
Dan Sanderson <contact@dansanderson.com>
2022-08-07 21:52:05 -0700

    wtf

--- confdefs.h
+++
@@ -1,7 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "m65tool"
-#define PACKAGE_TARNAME "m65tool"
-#define PACKAGE_VERSION "0.1"
-#define PACKAGE_STRING "m65tool 0.1"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_URL ""
